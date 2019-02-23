559b51134d353a7fad649f1b2b3a493845bf0953
SINBRO <shlapkoan@gmail.com>
2019-02-24 00:14:59 +0300

    test

--- app/src/main/java/sinbro/randomimages/ItemDetailActivity.kt
+++ app/src/main/java/sinbro/randomimages/ItemDetailActivity.kt
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_item_detail.*
 
-class ItemDetailActivity : AppCompatActivity() {
+class ItemDetailActivity : AppCompatActivity()  {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
