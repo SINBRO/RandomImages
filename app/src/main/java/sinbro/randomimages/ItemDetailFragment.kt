db7203b3b2a9b47bcde52decd685bdff544f5938
SINBRO <shlapkoan@gmail.com>
2019-02-24 03:22:20 +0300

    downloading with intent service

--- app/src/main/java/sinbro/randomimages/ItemDetailFragment.kt
+++ app/src/main/java/sinbro/randomimages/ItemDetailFragment.kt
@@ -1,15 +1,18 @@
 package sinbro.randomimages
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.os.AsyncTask
+import android.content.ComponentName
+import android.content.ServiceConnection
+import android.content.Intent
 import android.os.Bundle
+import android.os.IBinder
 import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.item_detail.*
 import kotlinx.android.synthetic.main.item_detail.view.*
-import java.io.IOException
+import sinbro.randomimages.loader.ImageLoader
 import java.lang.ref.WeakReference
 import java.net.URL
 
@@ -17,16 +20,45 @@ import java.net.URL
 class ItemDetailFragment : Fragment() {
     private var item: ItemListActivity.Item? = null
     private lateinit var rootView : View
+    private var serviceBind = false
+
+    private var binder: ImageLoader.MyBinder? = null
+
+    private val serviceConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName, service: IBinder) {
+            serviceBind = true
+            binder = service as ImageLoader.MyBinder
+
+            binder!!.setCallback { p ->
+                image.setImageBitmap(p)
+            }
+        }
+
+        override fun onServiceDisconnected(name: ComponentName) {
+            serviceBind = false
+            binder = null
+        }
+    }
+    companion object {
+        const val DESCRIPTION : String = "description"
+        const val IMAGE_URL : String = "imageUrl"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
-
         arguments?.let {
             if (it.containsKey(DESCRIPTION) && it.containsKey(IMAGE_URL)) {
                 item = ItemListActivity.Item(it[DESCRIPTION] as String, it[IMAGE_URL] as String)
+
+                if (!serviceBind) {
+                    val intent = Intent(context, ImageLoader::class.java)
+                    intent.putExtra(IMAGE_URL, item?.imageUrl)
+                    activity?.bindService(intent, serviceConnection, AppCompatActivity.BIND_AUTO_CREATE)
+                    activity?.startService(intent)
+                }
             }
+
         }
     }
 
@@ -37,13 +69,12 @@ class ItemDetailFragment : Fragment() {
 
         item?.let {
             rootView.description.text = it.text
-            DownloadAsyncTask(WeakReference(this)).execute(URL(it.imageUrl))
         }
 
         return rootView
     }
 
-    private class DownloadAsyncTask(val activity: WeakReference<ItemDetailFragment>) : AsyncTask<URL, Int, Bitmap>() {
+    /*private class DownloadAsyncTask(val activity: WeakReference<ItemDetailFragment>) : AsyncTask<URL, Int, Bitmap>() {
         override fun onPostExecute(result: Bitmap?) {
             activity.get()?.rootView?.image?.setImageBitmap(result)
         }
@@ -74,10 +105,13 @@ class ItemDetailFragment : Fragment() {
 
             return BitmapFactory.decodeByteArray(res, 0, res!!.size)
         }
-    }
+    }*/
 
-    companion object {
-        const val DESCRIPTION : String = "description"
-        const val IMAGE_URL : String = "imageUrl"
+    override fun onDestroy() {
+        super.onDestroy()
+        if (serviceBind) {
+            serviceBind = false
+            activity?.unbindService(serviceConnection)
+        }
     }
 }
